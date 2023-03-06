5554e1a6d043a88653b8f48b883fb4fe56c1f28d
Camilo-98 <ccninor@correo.udistrital.edu.co>
2023-03-06 14:54:18 -0500

    añadiendo rutas publicas y privadas

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,28 @@ import { Route, Routes } from "react-router-dom";
 import { LoginPage } from "../auth";
 import { RecordsRoutes } from "../records";
 import { Navbar } from "../ui";
+import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage/>}/>
-        <Route path="/*" element={<RecordsRoutes/>}/>
+
+        <Route path="login" element={
+          <PublicRoute>
+            <LoginPage/>
+          </PublicRoute>
+        } />
+
+        {/* <Route path="login" element={<LoginPage/>}/> */}
+
+        <Route path="/*" element={
+          <PrivateRoute>
+            <RecordsRoutes/>
+          </PrivateRoute>
+        }/>
+
       </Routes>
     </>
   )
